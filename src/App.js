6104929d69e7d40df584f927166b417b6c13e4a4
Charles Kiser <charles.kiser@improving.com>
2024-04-22 14:54:42 -0400

    Removing routing, adding redirect for netlify page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
 import TVShowCharacters from "./components/TVShowCharacters";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" Component={TVShowCharacters}></Route>
-        </Routes>
-      </BrowserRouter>
+      <TVShowCharacters />
     </div>
   );
 }
